fe186a779dc9cb6e64f3a69b1ce9fb980d9d1e32
Bill Mårtensson <bill@magictechnology.se>
2021-11-25 09:08:57 +0100

    Initial Commit

---
+++ piax25nov/piax25novApp.swift
@@ -0,0 +1,17 @@
+//
+//  piax25novApp.swift
+//  piax25nov
+//
+//  Created by Bill Martensson on 2021-11-25.
+//
+
+import SwiftUI
+
+@main
+struct piax25novApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
